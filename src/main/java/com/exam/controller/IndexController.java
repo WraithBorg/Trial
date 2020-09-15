995b3f32aa6a32e2ff2571877ac3070a66a1f081
Thanos <2325098522@qq.com>
2020-09-15 16:30:53 +0800

    逻辑优化

--- src/main/java/com/exam/controller/IndexController.java
+++ src/main/java/com/exam/controller/IndexController.java
@@ -4,13 +4,13 @@ import com.exam.entity.TrExam;
 import com.exam.mapper.TrExamMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -29,17 +29,23 @@ public class IndexController {
 
     @RequestMapping("/Next")
     public String Next(Model model) {
-        String maxId = trExamMapper.selectMaxId();
-        if (maxId == null){
+        Integer maxId = trExamMapper.selectCount(null);
+        if (maxId == null) {
             model.addAttribute("WarnInfo", "没有可用数据");
             return "index";
         }
-        Integer MaxId = Integer.parseInt(maxId);
-        int randomId = (int) (1 + Math.random() * (MaxId - 1 + 1));
-        TrExam trExamDB = trExamMapper.selectById(randomId);
-        model.addAttribute("ID", trExamDB.getId());
-        model.addAttribute("Question", trExamDB.getQuestion());
-        model.addAttribute("Answer", "");
+        int randomId = (int) (1 + Math.random() * (maxId - 1 + 1));
+        try {
+            List<String> ids = trExamMapper.getIds();
+            String id = ids.get(randomId - 1);
+            TrExam trExamDB = trExamMapper.selectById(id);
+            model.addAttribute("ID", trExamDB.getId());
+            model.addAttribute("Question", trExamDB.getQuestion());
+            model.addAttribute("Answer", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("WarnInfo", e.getMessage());
+        }
         return "index";
     }
 
@@ -52,11 +58,22 @@ public class IndexController {
         model.addAttribute("Answer", trExamDB.getAnswer());
         return "index";
     }
+
+    @RequestMapping("/Delete")
+    public String Delete() {
+        return "index";
+    }
+
+    @RequestMapping("/Answer")
+    public String Answer() {
+        return "index";
+    }
+
     @RequestMapping("/Delete/{ID}")
     public String Delete(@PathVariable String ID, Model model) {
         trExamMapper.deleteById(ID);
-        model.addAttribute("ID","");
-        model.addAttribute("Question","");
+        model.addAttribute("ID", "");
+        model.addAttribute("Question", "");
         model.addAttribute("Answer", "");
         return "index";
     }
@@ -67,11 +84,12 @@ public class IndexController {
     }
 
     @PostMapping("/AddQuestion")
-    public String AddQuestion(String Question,String Answer,Model model) {
-        try{
+    public String AddQuestion(String Question, String Answer, Model model) {
+        try {
+            Answer = Answer.replaceAll("\n", "<br>");
             TrExam trExam = new TrExam(Question, Answer);
             trExamMapper.insert(trExam);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             model.addAttribute("WarnInfo", e.getMessage());
         }
--- src/main/java/com/exam/mapper/TrExamMapper.java
+++ src/main/java/com/exam/mapper/TrExamMapper.java
@@ -4,7 +4,12 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.exam.entity.TrExam;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 public interface TrExamMapper extends BaseMapper<TrExam> {
     @Select("SELECT max(id) AS id FROM tr_exam")
     public String selectMaxId();
+
+    @Select("SELECT id FROM tr_exam order by id")
+    public List<String> getIds();
 }
